e93dc64248a278ea3fb5aea127b3671b9056f0f9
KingIsulgard <gilles.Lesire@gmail.com>
2015-03-22 16:58:44 +0100

    Create MultiValueTableViewController.h

---
+++ MultiValueTableViewController.h
@@ -0,0 +1,24 @@
+//
+//  MultiValueTableViewController.h
+//
+//  Created by Gilles Lesire on 16/07/14.
+//  GNU General Public License (GPL)
+//
+
+#import <UIKit/UIKit.h>
+#import "SettingsController.h"
+
+@protocol MultiValueDelegate
+
+- (void) selectedMultiValue;
+
+@end
+
+@interface MultiValueTableViewController : UITableViewController {
+    NSDictionary *prefSpecification;
+}
+
+@property (nonatomic) id<MultiValueDelegate> delegate;
+@property (strong, nonatomic) NSDictionary *prefSpecification;
+
+@end
